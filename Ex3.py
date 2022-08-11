a0dd68e9f9f3d0c9ef439670261bbc3a5775bb49
Ana Clara <mm.anaclara@gmail.com>
2022-08-11 11:12:50 -0300

    Delete Ex3.py

--- Ex3.py
+++
@@ -1,8 +0,0 @@
-faturamentos =[1,2,3,4,5,6]
-diassuperior=0
-media = sum(faturamentos)/len(faturamentos) #media mensal, pegando o tamanho ja excluimos os dias que nao tiveram faturamento
-for dias in range(len(faturamentos)):
-    if faturamentos[dias]>media:
-        diassuperior = diassuperior+1
-print("Abaixo temos o menor e maior valor de faturamento ocorrido em um dia do mês,e o número de dias no mês em que o valor de faturamento diário foi superior à média mensal.")
-print(min(faturamentos), max(faturamentos), diassuperior)
